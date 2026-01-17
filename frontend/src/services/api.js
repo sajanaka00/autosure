0ea998df5af1525d55396d41564ab5521790d87e
sajanakans <nivinsajanaka09@gmail.com>
2026-01-17 21:11:05 +0530

    updated the Signup and Signin pages to be more responsive

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // API Service
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'http://localhost:3001/api';
 
 class ApiService {
   async makeRequest(endpoint, options = {}) {
